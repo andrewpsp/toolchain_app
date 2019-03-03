4affb2456ed874309a22fdc1670cb80918290693
Andrew Thompson <admin@entercloud.info>
2019-03-03 03:06:50 -0600

    build enviroment initialized for spring app

---
+++ src/main/java/com/capgemini/pipeline_ui/PipelineUiApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.pipeline_ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelineUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipelineUiApplication.class, args);
+	}
+
+}
