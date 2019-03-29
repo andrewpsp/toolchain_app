653813a08d042b0947f92c5f20f9e8d862eba2e6
Andrew Thompson <admin@entercloud.info>
2019-03-29 09:39:58 -0500

    deploy new feature

---
+++ the_tests/new-feature.sh
@@ -0,0 +1,13 @@
+git clone https://github.com/spring-guides/gs-validating-form-input.git
+
+Go inside this folder
+gs-validating-form-input/complete  
+
+run the below maven command
+mvn clean install
+
+run the below command to start the app
+java -jar ./target/gs-validating-form-input-0.1.0.jar
+
+Open the browser with the below link
+http://localhost:8080/
